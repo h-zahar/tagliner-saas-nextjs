e47d9d4e4fc9d2607bd23bedcd2b8daedb8e5eba
h-zahar <hz.rifat@gmail.com>
2022-12-03 07:47:07 +0600

    Fixed Module Import Error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import TagLiner from '../components/tagliner'
+import TagLiner from '../components/tagLiner'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
