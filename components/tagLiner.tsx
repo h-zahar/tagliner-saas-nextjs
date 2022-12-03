8dcb0f753f15e2c59cfead15a6b49e3f0a4a9036
h-zahar <hz.rifat@gmail.com>
2022-12-03 07:37:41 +0600

    Initialized with API Integration

---
+++ components/tagLiner.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { useRef, useState } from "react";
+
+const TagLiner: React.FC = () => {
+    const [keyword, setKeyword] = useState('');
+    const inputRef = useRef<HTMLInputElement>(null);
+    const ENDPOINT = 'https://api-saas.onrender.com/snippet';
+    const [tagLine, setTagLine] = useState('');
+
+    const onsubmit = () => {
+        if (keyword === '') return;
+        inputRef.current ? inputRef.current.hidden = true : null;
+
+        fetch(`${ENDPOINT}?user_input=${keyword}`)
+        .then(res => res.json())
+        .then(data => {
+            setTagLine(data?.content);
+            setKeyword('');
+            inputRef.current ? inputRef.current.hidden = false : null;
+        })
+        .catch(err => {
+            console.log(err);
+            inputRef.current ? inputRef.current.hidden = false : null;
+        });
+    }
+    return (
+        <>
+            <div>
+                <h1>TagLiner</h1>
+                <p>Enter Keywords for your brand</p>
+            </div>
+            <div ref={inputRef}>
+            <input 
+                placeholder="tea" 
+                value={keyword}
+                onChange={(e) => setKeyword(e.currentTarget.value)} 
+                onKeyUp={(e) => {
+                    if (e.key === 'Enter') onsubmit();
+                }}
+            />
+            </div>
+            <button onClick={onsubmit}>Submit</button>
+            <div>
+                {
+                    tagLine !== '' ?
+                    <p>{tagLine}</p>
+                    : null
+                }
+            </div>
+        </>
+    );
+};
+
+export default TagLiner;
